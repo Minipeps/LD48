55757b4a712e35d4fbfea7246321960286325cbb
Kazoo <tchoopatchoop@gmail.com>
2021-04-24 15:06:09 +0200

    Create CharacterFactory

---
+++ Assets/CharacterFactory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFactory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("CharacterFactory loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
