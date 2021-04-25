1a00931163d7ee00097813997825a8097e0c272b
RThomas96 <thomas.richard10@free.fr>
2021-04-25 11:06:45 +0200

    Fix starting level

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -32,7 +32,7 @@ public class GameMaster : MonoBehaviour
     void Start()
     {
         currentScore = 0;
-        currentLevel = Level.Level3;
+        currentLevel = Level.Level1;
 	audioManager.playAmbience(currentLevel);
         SwitchToNewCharacter();
     }
