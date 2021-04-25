702fa77e7ef9d94018d7ca2fb24505fafd83c6c2
Kazoo <tchoopatchoop@gmail.com>
2021-04-25 09:01:01 +0200

    Create SettingsManager

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public bool soundEnabled = true;
+    public bool musicEnabled = true;
+    public string langage = "en";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
