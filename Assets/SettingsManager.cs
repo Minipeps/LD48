bf7b8f31076f1543a4ef9648b11f77aeadfd8902
Minipeps <mpcaudan@gmail.com>
2021-04-25 16:10:31 +0200

    Add more menu options & wire sound button

--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
+    public Text playButton;
+    public Text rulesButton;
     public Text languageButton;
+    public Text soundButton;
+    public Text quitButton;
 
     public bool soundEnabled = true;
     public bool musicEnabled = true;
@@ -30,15 +34,35 @@ public class SettingsManager : MonoBehaviour
 
     public void SwitchLanguage()
     {
+        language = isFrench() ? "en" : "fr";
+        UpdateButtonsText();
+    }
+
+    public void ToggleSound()
+    {
+        soundEnabled = !soundEnabled;
+        musicEnabled = !musicEnabled;
+
+        soundButton.text = (isFrench() ? "Son" : "Sound") + ": " + (soundEnabled ? "On" : "Off");
+    }
+
+    private void UpdateButtonsText()
+    {
         if (isFrench())
         {
-            language = "en";
-            languageButton.text = "Language: English";
+            playButton.text = "Jouer!";
+            rulesButton.text = "Règles";
+            languageButton.text = "Langue: Français";
+            soundButton.text = "Son: " + (soundEnabled ? "On" : "Off");
+            quitButton.text = "Quitter";
         }
         else
         {
-            language = "fr";
-            languageButton.text = "Langue: Français";
+            playButton.text = "Play!";
+            rulesButton.text = "Rules";
+            languageButton.text = "Language: English";
+            soundButton.text = "Sound: " + (soundEnabled ? "On" : "Off");
+            quitButton.text = "Quit";
         }
     }
 }
